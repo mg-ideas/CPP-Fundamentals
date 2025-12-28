2694f5ceb710d6d277bb87030db60d7970812bd4
Ram Basnet <rambasnet@gmail.com>
2025-12-27 22:55:44 -0700

    add stdio lab and kattis problems

---
+++ labs/stdio/echoechoecho/echoechoecho.cpp
@@ -0,0 +1,32 @@
+/*
+    StdIO Lab
+    Kattis - Echo Echo Echo Problem
+
+    Updated By: <FIXME>
+    Date: <FIXME>
+    CSCI 111
+
+    This program prints the input word 3 times.
+
+    Algorithm steps:
+    1. Declare a string variable to store data
+    3. Read the word into the variable
+    3. Print the word 3 times separated by space
+*/
+
+#include <iostream>
+// FIXME1: include strinG library
+// FIXME2: use namespace standard
+
+int main()
+{
+    // FIXME3: Uncomment the following two lines
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+
+    // FIXME4: step 1
+    // FIXME5: step 2
+    // FIXME6: complete algorithm step 3
+
+    return 0;
+}
