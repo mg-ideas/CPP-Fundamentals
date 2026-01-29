a41a730f5bc639f33491f5df666b8aa5a8ad5c9a
Ram Basnet <rambasnet@gmail.com>
2026-01-28 21:50:34 -0700

    update stdio lab

--- labs/stdio/echoechoecho/echoechoecho.cpp
+++ labs/stdio/echoechoecho/echoechoecho.cpp
@@ -9,13 +9,13 @@
     This program prints the input word 3 times.
 
     Algorithm steps:
-    1. Declare a string variable to store data
-    3. Read the word into the variable
+    1. Declare a string variable to store input data
+    3. Read and store the input data into the variable
     3. Print the word 3 times separated by space
 */
 
 #include <iostream>
-// FIXME1: include strinG library
+// FIXME1: include string library
 // FIXME2: use namespace standard
 
 int main()
@@ -24,9 +24,9 @@ int main()
     // ios_base::sync_with_stdio(false);
     // cin.tie(NULL);
 
-    // FIXME4: step 1
-    // FIXME5: step 2
-    // FIXME6: complete algorithm step 3
+    // FIXME4: algorithm step 1
+    // FIXME5: algorithm step 2
+    // FIXME6: algorithm step 3
 
     return 0;
 }
