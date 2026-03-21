0464f000bedb6c1f8348fdaa6b9ecbde3d8b19b2
Ram Basnet <rambasnet@gmail.com>
2026-03-20 20:35:30 -0600

    update fileio lab

--- labs/fileio/tests/test_fileio.cpp
+++ labs/fileio/tests/test_fileio.cpp
@@ -6,13 +6,15 @@ TEST_CASE("Testing read_data function")
 {
     SUBCASE("Sub Case 1: Valid file with numbers")
     {
-        // assume data/input1.txt exists with 7 test data
-        int *
-            string input_file = "data/input1.txt"; // make sure this file exists with test data
-        read_data(numbers, input_file);
-        CHECK(numbers.size() == 7); // assuming input1.txt has 7 numbers
-        CHECK(numbers[0] == 100);   // adjust expected values as per your input test file
+        // assume data/sample_input.txt exists with 7 test data
+        int *numbers = new int[1000];                // assuming we will read at most 1000 numbers from the file
+        string input_file = "data/sample_input.txt"; // make sure this file exists with test data
+        size_t arr_len = read_data(numbers, input_file);
+        CHECK(arr_len == 7);      // assuming sample_input.txt has 7 numbers
+        CHECK(numbers[0] == 100); // adjust expected values as per your input test file
         CHECK(numbers[1] == 10);
+        CHECK(numbers[arr_len - 1] == 99);
+        delete[] numbers;
     }
     // FIXME7: Write 2nd test case for read_data function
 }
@@ -21,9 +23,9 @@ TEST_CASE("Testing find_min function")
 {
     SUBCASE("Sub Case 1: Distinct values")
     {
-        vector<int> numbers = {5, 3, 8, 1, 4};
+        int numbers[] = {5, 3, 8, 1, 4};
         int expected = 1;
-        int result = find_min(numbers);
+        int result = find_min(numbers, 5);
         CHECK(result == expected);
     }
     // FIXME8: Write 2nd test case for find_min function
@@ -33,10 +35,11 @@ TEST_CASE("Testing find_range function")
 {
     SUBCASE("Sub Case 1: Distinct values")
     {
-        vector<int> numbers = {5, 3, 8, 1, 4};
+        int *numbers = new int[5]{5, 3, 8, 1, 4};
         int expected = 7; // 8 - 1
-        int result = find_range(numbers);
+        int result = find_range(numbers, 5);
         CHECK(result == expected);
+        delete[] numbers;
     }
     // FIXME9: Write 2nd test case for find_range function
 }
@@ -45,9 +48,9 @@ TEST_CASE("Testing find_mean function")
 {
     SUBCASE("Sub Case 1: Distinct values")
     {
-        vector<int> numbers = {5, 3, 8, 1, 4};
+        int numbers[] = {5, 3, 8, 1, 4};
         float expected = 4.2f; // (5+3+8+1+4)/5
-        float result = find_mean(numbers);
+        float result = find_mean(numbers, 5);
         CHECK(fabs(result - expected) <= EPSILON);
     }
     // FIXME10: Write 2nd test case for find_mean function
@@ -57,9 +60,9 @@ TEST_CASE("Testing find_median function")
 {
     SUBCASE("Sub Case 1: Odd number of elements")
     {
-        vector<int> numbers = {5, 3, 8, 1, 4};
+        int numbers[] = {5, 3, 8, 1, 4};
         float expected = 4.0f; // sorted: {1,3,4,5,8}
-        float result = find_median(numbers);
+        float result = find_median(numbers, 5);
         CHECK(fabs(result - expected) <= EPSILON);
     }
     // FIXME11: Write 2nd test case for find_median function
